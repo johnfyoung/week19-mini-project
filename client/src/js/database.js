a138e648bcffb845a78f5c9c20f0e93e0a0cf225
John F Young <john@codeandcreative.com>
2023-02-19 19:03:10 -0800

    Working build without SW and IDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,16 @@
 // TODO: Install the following package:
-import { openDB } from 'idb';
+// import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
-
-};
-
+const initdb = async () => {};
 
 // TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
-};
+export const postDb = async (name, home, cell, email) => {};
 
 // TODO: Complete the getDb() function below:
-export const getDb = async () => {
-  
-};
+export const getDb = async () => {};
 
 // TODO: Complete the deleteDb() function below:
-export const deleteDb = async (id) => {
-  
-};
+export const deleteDb = async (id) => {};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,27 +1,41 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      cards: "./src/js/cards.js",
     },
 
     // TODO: Add the correct output
     output: {
-      
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
-    }
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
   };
 };
